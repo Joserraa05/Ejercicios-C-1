5989f6b10a6f14e7109fc2290a15ba762d7427fa
Joserraa05 <juke15yt@gmail.com>
2025-10-22 13:29:07 +0200

    Ejercicios de WPF2

---
+++ Ejercicios WPF2/WPF2-Ejercicio6/WPF2-Ejercicio6/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF2_Ejercicio6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void entraRaton(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(btn1.Foreground.ToString());
+        }
+    }
+}
---
+++ Ejercicios WPF2/WPF2-Ejercicio8/WPF2-Ejercicio8/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF2_Ejercicio8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TodoCheck_Check(object sender, EventArgs e)
+        {
+            bool newVal = (TodoCheck.IsChecked == true);
+            di.IsChecked = newVal;
+            sge.IsChecked = newVal;
+            ad.IsChecked = newVal;
+        }
+        private void check(object sender, EventArgs e)
+        {
+            TodoCheck.IsChecked = null;
+            if ((di.IsChecked == true) && (sge.IsChecked == true) && (ad.IsChecked == true))
+            { TodoCheck.IsChecked= true; }
+            if ((di.IsChecked == false) && (sge.IsChecked == false) && (ad.IsChecked == false))
+            { TodoCheck.IsChecked= false; }
+        }
+    }
+}
