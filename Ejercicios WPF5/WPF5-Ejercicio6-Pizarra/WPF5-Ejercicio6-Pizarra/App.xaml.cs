d4ab146b1f926e977eb2dfa1e19ae1ec4903e64d
Joserraa05 <juke15yt@gmail.com>
2025-10-29 14:14:46 +0100

    Ejercicios 5 de WPF

---
+++ Ejercicios WPF5/WPF5-Ejercicio6-Pizarra/WPF5-Ejercicio6-Pizarra/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF5_Ejercicio6_Pizarra
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
