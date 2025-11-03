ab4ccca7d7cd2cd138c38fcc7c5af383ef6d9b04
Joserraa05 <juke15yt@gmail.com>
2025-11-03 11:03:32 +0100

    WPF Relación de ejercicios nº6

---
+++ Ejercicios WPF6/WPF6-Ejercicio1/WPF6-Ejercicio1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF6_Ejercicio1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Ejercicios WPF6/WPF6-Ejercicio3/WPF6-Ejercicio3/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF6_Ejercicio3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Enviar(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("El usuario " + txtNombre.Text + " con email " + txtEmail.Text + " envía el mensaje " + txtMensaje.Text);
+        }
+    }
+}
